54886feb5e923633a16e3cc670e1fce656cafbd9
Julie Dallet <simon.dallet@etu.uca.fr>
2021-11-17 20:24:17 +0100

    Debug mini bug affichage + fonction pho à pho ameliorée

--- contrepeterie/suggestion.js
+++ contrepeterie/suggestion.js
@@ -80,51 +80,54 @@ function motExiste(mot, dic){
 	return false;
 }
 
-/*function onBtClick(){
-	let mot = document.getElementById('mot');	
-	suggestionMot();
-}*/
 
 //fonction permettant la contrepétrie à base de phonèmes, échange d'1 phonème contre 1 phonème
 function aidePhonemeSubs(){
+	affichResultat=[];
 	var l=[];
 	let mo = document.getElementById('mot').value;
 	mot=mo.toLowerCase();
 	console.log("mot :" + mot);
-	//console.log(dicMot);
-	let ind = 0; 
-	console.log(mot + dicMot[3]);
+	if (mot.length == 0)
+		return
+	let ind = 0;
 	for(let j=0;j<dicMot.length;j++){
 		if(dicMot[j] == mot){
 			ind = j;
 		}
 	}
-	mot2=dicPhon[ind];
-	//var alph=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+	if (ind==0) {
+		l.push("Aucune correspondance");
+		affichageMot(l);
+		l=[];
+		return;
+	}
+	console.log("ind :" + ind);
+	var mot2=dicPhon[ind];
+	console.log("mot2 :" + mot2);
 	var alph = ['b', 'd', 'f', 'g', 'k', 'l', 'm', 'n', 'ŋ','ɲ','p','ʁ','s', 'ʃ', 't', 'v', 'z', 'ʒ', 'j', 'w','ɥ', 'a', 'ɑ', 'e', 'ɛ','ɛː','ə','i', 'œ','ø','o','ɔ','u','y','ɑ̃','ɛ̃','œ̃','ɔ̃'];
+	let motSave=mot2; //On garde le mot en memoire
 	var m=mot2.split('');
 	console.log(m);
-	
-	for(var i=0; i<m.length; i++){
-		console.log(m[i]);
-		for(var j=0;j<alph.length;j++){
-			var mcopy=m.slice(0);
-			mcopy[i]=alph[j];
-			//console.log(mcopy);
-			var mcopyjoin=mcopy.join('');
-			console.log(mcopyjoin);
-			if(motExiste(mcopyjoin,dicPhon) && mcopyjoin != mot){
-				console.log('Ok');
-				let i = dicPhon.indexOf(mcopyjoin);
+
+	for (let i=0; i<mot2.length; i++) { //Pour chaque lettre de notre mot
+		mot2=motSave; //On reinitialise le mot ici afin d'avoir toujours "code" au lieu de "zode" puis "zzode" par ex
+		for(let j=0;j<alph.length;j++) { //Pour chaque lettre de l'alphabet
+			mot2 = mot2.replaceAt(i,alph[j]); //On remplace la lettre du mot par la lettre de l'alphabet
+			console.log(mot2);
+			if (motExiste(mot2,dicPhon) && mot2 != mot) { //Si le mot existe et que le mot n'est pas le mot saisi
+				console.log('Ok : ' + mot2 + " ajouté");
+				let i = dicPhon.indexOf(mot2);
 				if(dicMot[i] != mot){
-					l.push(dicMot[i]);
+					l.push(dicMot[i]); //On ajoute le mot dans la liste l des mots compatibles
 				}
-				
 			}
 		}
+
 	}
+	console.log("liste mot compatible " + l);
 	choixMotCompatible(mot, l);
+
 }
 
 
@@ -188,6 +191,12 @@ function aideLettreSubs() {
 		if(dicMot[j] == mot){
 			ind = j;
 		}
+	}
+	if (ind==0) {
+		l.push("Aucune correspondance");
+		affichageMot(l);
+		l=[];
+		return;
 	}
 	var mot2=dicMot[ind]; //On copie ce mot dans mot2
 	var alph=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
