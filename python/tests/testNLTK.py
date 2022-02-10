c2b14db5066e16abce25ad1a93e9e2660025364e
loperret2 <louis.perret@etu.uca.fr>
2022-02-10 22:32:29 +0100

    fonctions pour vérifier l'écriture phonétique et récupérer la pluralité d'un mot fonctionneles pour le français

--- python/tests/testNLTK.py
+++ python/tests/testNLTK.py
@@ -1,8 +1,9 @@
 import nltk
 #nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
-#ltk.download('tagsets')
-
+#nltk.download('tagsets')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
 from nltk.tokenize import word_tokenize,sent_tokenize
 
 
@@ -11,7 +12,7 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.tokenize import sent_tokenize
 """
-sentence="j'ai une belle frite et vous "
+sentence="j'ai de belles frites et vous "
 sentence=word_tokenize(sentence,language="french")
 #entence=nltk.pos_tag(sentence)
 #nltk.help.upenn_tagset()
@@ -23,4 +24,32 @@ model = '../nltk/stanford-postagger-full-2020-11-17/models/french-ud.tagger'
 
 pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8' )
 res = pos_tagger.tag(sentence)
-print (res)
+#print (res)
+
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.snowball import FrenchStemmer
+wnl = WordNetLemmatizer()
+
+def isplural(word):
+    lemma = wnl.lemmatize(word, 'n')
+    plural = True if word is not lemma else False
+    return plural, lemma
+
+"""
+nounls = ['geese', 'mice', 'bars', 'foos', 'foo', 
+              'families', 'family', 'dog', 'dogs']
+              """
+nounls = ['chiens', "mères", 'maires', 'homme', 'doigt', 
+              'orteils', "maison", 'maisons', 'aventuriers']
+"""
+for nn in nounls:
+    isp, lemma = isplural(nn)
+    print(nn, lemma, isp)
+"""
+"""
+import treetaggerwrapper
+tagger = treetaggerwrapper.TreeTagger(TAGLANG='en')
+tags = tagger.tag_text("pip install treetaggerwrapper")
+print(tags)
+"""
+#---------------------------------------
