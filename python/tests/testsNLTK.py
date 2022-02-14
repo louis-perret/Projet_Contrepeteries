8ace81f3c2f33e60f627a5afadb4c90201e363f3
loperret2 <louis.perret@etu.uca.fr>
2022-02-14 11:59:25 +0100

    création branche pour testNLTK + continuation des tests NLTK avec des patterns simples

--- python/tests/testsNLTK.py
+++ python/tests/testsNLTK.py
@@ -1,17 +1,71 @@
 import nltk
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('tagsets')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
+#nltk.download('conll2000')
 
 from nltk.tokenize import word_tokenize
 from nltk.tag import StanfordPOSTagger
 jar = '../nltk/stanford-postagger-full-2020-11-17/stanford-postagger-4.2.0.jar'
 model = '../nltk/stanford-postagger-full-2020-11-17/models/french-ud.tagger'
 
-lotr_quote = "je regarde une jolie personne"
+#Tests
+tabPhrases=[]
+tabPhrases.append("je regarde une jolie personne") #marche
+tabPhrases.append("je une jolie personne") #marche pas
+tabPhrases.append("je regarde une personne") #marche
+tabPhrases.append("la personne regarde") #marche
+tabPhrases.append("je regarde personne") #marche pas
+tabPhrases.append("regarde une jolie personne") #marche pas
+tabPhrases.append("une regarde jolie personne") #marche pas -> il dit que ça marche alors qu'elle est fausse -> faudra faire des règles plus précises
 
-words_in_lotr_quote = word_tokenize(lotr_quote, language="french")
 pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8' )
-lotr_pos_tags = pos_tagger.tag(words_in_lotr_quote)
-print(lotr_pos_tags)
-grammar = "NP: {<DET>?<NOUN>*<ADJ>}"
-chunk_parser = nltk.RegexpParser(grammar)
-tree = chunk_parser.parse(lotr_pos_tags)
-tree.draw()
+for i in range(len(tabPhrases)): #pour chaque phrase on tokenize, puis on tag
+    tabPhrases[i] = word_tokenize(tabPhrases[i], language="french")
+    tabPhrases[i] = pos_tagger.tag(tabPhrases[i])
+#print(lotr_pos_tags)
+
+
+#test avec des patterns simples
+def testChunking(tabPhrases):
+    grammar = """
+                NP: {<DET>?<NOUN><ADJ>}
+                NP: {<DET>?<ADJ>*<NOUN>}
+                CP: {<PRON|DET|NP><VERB><NP>?}
+                """ #définis un chunck
+
+
+    chunk_parser = nltk.RegexpParser(grammar)
+    for i in range(len(tabPhrases)):
+        tree=chunk_parser.parse(tabPhrases[i])
+        b=False
+        tabElements=tree.productions()[0].rhs() #on récupère la partie droite du premier résultat du parsage 
+        print(tabElements)
+        for element in tabElements: #on parcours les éléments
+            if(isinstance(element,nltk.grammar.Nonterminal)): #si c'est un élément non terminal et non un tuple
+                if(element.symbol() == "CP"): #s'il contient l'élément qu'on a voulu vérifier
+                    b=True
+
+        if(b): #si true -> phrase juste
+            print(f"Phrase : {tabPhrases[i]} -> juste")
+        else:
+            print(f"Phrase : {tabPhrases[i]} -> pas juste")
+
+    return tree
+
+#Marche pas
+def accuracy(phrase):
+    from nltk.corpus import conll2000
+    grammar = r"NP: {<DET>?<ADJ>*<NOUN>}"
+    cp = nltk.RegexpParser(grammar)
+    phrase=conll2000.chunked_sents(phrase, chunk_types=['NP'])
+    print(cp.evaluate(phrase))
+
+
+testChunking(tabPhrases)
+#accuracy(phrase)
+
+
+
