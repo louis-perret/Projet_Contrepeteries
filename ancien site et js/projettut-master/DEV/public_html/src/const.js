69b098e5862121105ead03b9f8da1f3f7ff23cb5
LouisPerret <louisperret0311@gmail.com>
2022-04-12 22:02:09 +0200

    Suppressions des anciens projets

--- ancien site et js/projettut-master/DEV/public_html/src/const.js
+++
@@ -1,3 +0,0 @@
-const pathToDictionnary = 'https://sancy.iut-clermont.uca.fr/~lafourcade/contrepeteries/dict_fr_ok.csv';
-const orange = '#FFA600';
-const green = '#28a745';
--- ancien site et js/projettut-master/DEV/res/dict_en.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const fs = require('fs');
-
-fs.readFile('dict_en.csv', 'utf8', function (err, data) {
-  var dataArray = data.split(/\r?\n/);
-  console.log(dataArray);
-});
